59beae5b20bf3f3d80b12d98eccd599c3691c580
ragavim24it-debug <ragavim24it@psnacet.edu.in>
2026-08-11 15:45:52 +0530

    Update routes.py

--- backend/backend/routes.py
+++ backend/backend/routes.py
@@ -1,7 +1,68 @@
-from flask import jsonify
+from flask import Blueprint, request, jsonify
 
+from database import (
+    create_database,
+    insert_traffic,
+    get_all_traffic
+)
+
+
+traffic_routes = Blueprint(
+    "traffic_routes",
+    __name__
+)
+
+
+# Create database
+create_database()
+
+
+# GET all traffic data
+@traffic_routes.route(
+    "/api/traffic",
+    methods=["GET"]
+)
 def get_traffic():
+
+    data = get_all_traffic()
+
     return jsonify({
         "status": "success",
-        "message": "Traffic data loaded"
+        "count": len(data),
+        "data": data
     })
+
+
+# ADD traffic data
+@traffic_routes.route(
+    "/api/traffic",
+    methods=["POST"]
+)
+def add_traffic():
+
+    data = request.get_json()
+
+    road = data.get("road")
+    vehicles = data.get("vehicles")
+    speed = data.get("speed")
+    traffic_level = data.get("traffic_level")
+    delay = data.get("delay")
+
+    if not road:
+        return jsonify({
+            "status": "error",
+            "message": "Road name is required"
+        }), 400
+
+    insert_traffic(
+        road,
+        vehicles,
+        speed,
+        traffic_level,
+        delay
+    )
+
+    return jsonify({
+        "status": "success",
+        "message": "Traffic data stored successfully"
+    }), 201
